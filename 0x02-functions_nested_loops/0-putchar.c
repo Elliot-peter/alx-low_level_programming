bf9c9031e8aa79d0ee8174c0a94df72e0de9a465
Peter Yibaebele <peteryibaebele@gmail.com>
2022-10-19 08:24:10 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,33 @@
-#include "_main.h"
-/**
-  * main - print _putchar
-  *
-  * Return: Always 0 (Success)
-  */
-int main(void)
-{
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
+#include "main.h"
 
-	return (0);
+
+/**
+* main - Prints "putchar" followed by a new line.
+*
+* Return: Always 0.
+*/
+
+
+
+int main(void)
+
+{
+
+
+
+			_putchar('_');
+			_putchar('p');
+			_putchar('u');
+			_putchar('t');
+			_putchar('c');
+			_putchar('h');
+			_putchar('a');
+			_putchar('r');
+			_putchar('\n');
+
+
+			return (0);
+
+
+
 }
