45e3fa11a37913e3d58ae5b46c2d8e91d5b808d0
Peter Yibaebele <peteryibaebele@gmail.com>
2022-11-24 05:31:51 -0800

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+/**
+* turtle - prints before main
+*/
+
+void __attribute__ ((constructor)) turtle()
+{
+printf("You're beat! and yet, you must allow");
+printf(",\nI bore my house upon my back!\n");
+return (0);
+}
+``
