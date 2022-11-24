8c4e82f6acd00baa9ff0b392184d63ca87e210f4
Peter Yibaebele <peteryibaebele@gmail.com>
2022-11-24 05:45:56 -0800

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -10,6 +10,5 @@ void __attribute__ ((constructor)) turtle()
 {
 printf("You're beat! and yet, you must allow");
 printf(",\nI bore my house upon my back!\n");
-return (0);
 }
-``
+
--- 0x12-singly_linked_lists/100-main.c
+++ 0x12-singly_linked_lists/100-main.c
@@ -7,8 +7,8 @@
 */
 int main(void)
 {
-printf("(A tortoise, having pretty good sense of a
-hare's nature, challenges one to a race.)\n");
+printf("A tortoise, having pretty good sense of a");
+printf("hare's nature, challenges one to a race.)\n");
 return (0);
 }
 
